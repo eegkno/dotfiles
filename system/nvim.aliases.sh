b6aa2c2624dad8b3b4153fd7e3cf0bd4dbfccbe2
Edgar Garcia Cano <eegkno@gmail.com>
2019-04-18 21:15:50 -0400

    Update files for installing vim

---
+++ system/nvim.aliases.sh
@@ -0,0 +1,11 @@
+# Configuration variables
+export VIMCONFIG=~/.vim
+export VIMDATA=~/.vim
+
+# Use Neovim as "preferred editor"
+export VISUAL=nvim
+
+# Use Neovim instead of Vim or Vi
+alias vim=nvim
+alias vi=nvim
+
