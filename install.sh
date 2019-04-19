f4c3d3b7fc5f1d0d05e07c8b5ab11d3c76d0cd5e
Edgar Garcia Cano Castillo <edgar.garcia.cano.castillo@intact.net>
2019-04-19 10:11:03 -0400

    Add configuration for nvim and tmux

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-ln -s .dotfiles/tmux/tmux.conf .tmux.conf 
+
+# Link tmux
+ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf 
+
+# Link nvim
+mkdir -p .vim/.backup
+mkdir -p .vim/.swp
+mkdir -p .vim/.undo
+mkdir -p .vim/pack/bundle
+mkdir -p .vim/pack/minpac/opt
+mkdir -p .vim/pack/minpac/start
+
+ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/.dotfiles/vim/init.vim $HOME/.config/nvim/init.vim 
