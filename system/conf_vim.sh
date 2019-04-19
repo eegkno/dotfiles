a3b19eb9e8f69b35c04a6fb42d9b98a651964456
Edgar Garcia Cano Castillo <edgar.garcia.cano.castillo@intact.net>
2019-04-19 10:09:30 -0400

    Configuration were moved to base dir

--- system/conf_vim.sh
+++
@@ -1,9 +0,0 @@
-# Create Nvim required directories
-mkdir -p ~/.config/nvim
-
-# Copy the vim configuration file
-ln -s ~/Dropbox/_Git_repositories/dotfiles/vim/vimrc ~/.vimrc
-
-# Copy the nvim configuration file.
-# This will load the .vimrc as a configuration file for nvim  
-cp ../vim/init.vim ~/.config/nvim
