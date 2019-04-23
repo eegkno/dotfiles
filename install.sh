6a58a7ca8319605eb49286863608556ba8176606
Edgar Garcia Cano <eegkno@gmail.com>
2019-04-22 23:04:29 -0400

    Update installation script

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
-
 # Link tmux
+echo "Linking tmux configuration files"
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf 
 
 # Link nvim
+echo "creating vim directories"
 mkdir -p .vim/.backup
 mkdir -p .vim/.swp
 mkdir -p .vim/.undo
 mkdir -p .vim/pack/bundle
 mkdir -p .vim/pack/minpac/opt
 mkdir -p .vim/pack/minpac/start
-
-ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
-
 mkdir -p $HOME/.config/nvim
+
+echo "Linking vim configuration files"
+ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/vim/init.vim $HOME/.config/nvim/init.vim 
+
+echo "Copying git files"
+cp $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+cp $HOME/.dotfiles/git/.gitignore_global $HOME/.gitignore_global
+
+printf "Setting up Git...\\n\\n"
+
+defaultName=$( git config --global user.name )
+defaultEmail=$( git config --global user.email )
+defaultGithub=$( git config --global github.user )
+
+read -rp "Name [$defaultName] " name
+read -rp "Email [$defaultEmail] " email
+read -rp "Github username [$defaultGithub] " github
+
+git config --global user.name "${name:-$defaultName}"
+git config --global user.email "${email:-$defaultEmail}"
+git config --global github.user "${github:-$defaultGithub}"
+
