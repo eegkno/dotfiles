efbbd8ba0a44d2e4e6e0c28e80d9ba714a0446fa
Edgar Garcia Cano <eegkno@gmail.com>
2021-05-03 00:40:33 -0400

    Update shell configs

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,13 +1,19 @@
 # General aliases
 
-alias ll='ls -alFh'
+alias ll='ls -alFht'
 alias la='ls -A'
 alias l='ls -CF'
+
 alias tmux="TERM=screen-256color-bce tmux"
 alias dev='$HOME/.dotfiles/tmux/tmux_envs.sh dev 1'
 alias dev1='$HOME/.dotfiles/tmux/tmux_envs.sh dev1 1'
 alias dev2='$HOME/.dotfiles/tmux/tmux_envs.sh dev2 1'
 alias runpy='$HOME/.dotfiles/tmux/tmux_envs.sh runpy'
+
 alias ..='cd ../../'
 alias ...='cd ../../../'
 alias tree='tree --dirsfirst --charset=ascii'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
